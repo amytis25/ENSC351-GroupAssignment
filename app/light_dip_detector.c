fb0e8173b69718779c234ef12ae1790d9a57b7f6
Amytis Saghafi <amytis25@gmail.com>
2025-10-26 17:09:18 -0700

    made some new files

---
+++ app/light_dip_detector.c
@@ -0,0 +1,13 @@
+#include "hal/timing.h"
+#include "hal/sampler.h"
+#include "hal/SPI.h"
+#include "hal/periodTimer.h"
+#include <stdio.h> // fopen, fprintf, fclose, perror
+#include <stdlib.h>  // exit, EXIT_FAILURE, EXIT_SUCCESS
+#include <stdbool.h>
+#include <time.h>
+
+int main() {
+
+    return 0;
+}
