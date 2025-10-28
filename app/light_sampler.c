b0060b7a10088544ad190df1fbc10b6b7a9eae40
freemefromundergrad <melissamosavi@gmail.com>
2025-10-28 16:12:38 -0700

    cleaned up direcotry

--- app/light_sampler.c
+++ app/light_sampler.c
@@ -19,3 +19,16 @@
         - helper function in main function? 
 */
 
+#include "hal/timing.h"
+#include "hal/sampler.h"
+#include "hal/SPI.h"
+#include "hal/periodTimer.h"
+#include <stdio.h> // fopen, fprintf, fclose, perror
+#include <stdlib.h>  // exit, EXIT_FAILURE, EXIT_SUCCESS
+#include <stdbool.h>
+#include <time.h>
+
+int main() {
+
+    return 0;
+}
