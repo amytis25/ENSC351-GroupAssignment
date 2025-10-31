cc256d5fe66bfe81b95ffe96151d9c9c5f8b58e5
freemefromundergrad <melissamosavi@gmail.com>
2025-10-30 18:15:21 -0700

    undo noworky changes

--- app/noworky.c
+++ app/noworky.c
@@ -4,8 +4,6 @@
  * This program tries to create two arrays of data, and then swap their
  * contents. However, not all seems to go according to plan...
  */
-
- /* Changed line 29, error of underflowing. changed from unsigned to signed int*/
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -26,7 +24,7 @@ void swapContent(double *d1, double *d2)
  */
 void tradeArrays(double *array1, double *array2, int size)
 {
-	int i; // Changed from unsigned int to int
+	unsigned int i;
 	for (i = size-1; i >= 0; i--) {
 		swapContent(array1+i, array2+i);
 	}
